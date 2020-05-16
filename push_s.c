2a8893e02ec6c452da7edee69bd4acafbfe38c05
laucavv <laucavv23@gmail.com>
2020-05-16 17:05:06 +0000

    challenge

---
+++ push_s.c
@@ -0,0 +1,36 @@
+#include "monty.h"
+/**
+ * push_s - pushes an element to the stack
+ * @stack: stack
+ * @line_number: line number
+ * Return: void
+ */
+void push_s(stack_t **stack, unsigned int line_number)
+{
+	stack_t *end_int = NULL, *tmp;
+
+	end_int = malloc(sizeof(stack_t));
+
+	if (end_int == NULL)
+	{
+		return;
+	}
+	end_int->n = line_number;
+	end_int->next = NULL;
+	if (*stack == NULL)
+	{
+		*stack = end_int;
+		
+	}
+	else
+	{
+		tmp = *stack;
+		while (tmp->next != NULL)
+		{
+			tmp = tmp->next;
+		}
+		tmp->next = end_int;
+	}
+	printf("llego\n");
+
+}
